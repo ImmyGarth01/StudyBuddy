5937418b30537c8d8e510ee29612e4bad9ece7aa
spanda79 <pandas@roehampton.ac.uk>
2026-03-06 10:28:42 +0000

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -4,8 +4,12 @@ const db = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: 'password',        // or your MySQL password
-  database: 'studybuddy',  // your database name
+  database: 'Studybuddy',  // your database name
+  port:3308,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
+
+module.exports = db;
+
